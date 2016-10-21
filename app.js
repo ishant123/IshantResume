408d8b83f1e44207acda9799a2e46af27e8a695f
ishant123 <ishant.wadhwa5@gmail.com>
2016-10-21 20:36:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.listen(port,function(error){
 
 });
 app.get('/',function(req,res){
-    res.redirect('public/index.html');
+  return res.redirect('public/index.html');
 });
 
 app.post('/sendEmail',upload.array(),function(req,res){
